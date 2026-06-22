992e83d865f74ed96255eb414eb65e05fae4e075
kalyan-1845 <prsnlkalyan@gmail.com>
2026-06-22 11:28:22 +0530

    fix: resolve eslint no-explicit-any error in SupplyChainMap

--- src/components/SupplyChainMap.tsx
+++ src/components/SupplyChainMap.tsx
@@ -6,6 +6,7 @@ import { Badge } from '@/components/ui/badge';
 import { format } from 'date-fns';
 
 // Fix for default marker icon in react-leaflet
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
