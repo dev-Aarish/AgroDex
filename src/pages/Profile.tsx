7dc830f74eea41e93b2cbf06c73a87cefbc7fc6a
daviddprtma <davidchristianpratama@gmail.com>
2026-06-29 10:08:09 +0700

    update profile done

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -18,6 +18,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Helmet } from "react-helmet-async";
 import { DeleteProfileModal } from "@/components/DeleteProfileModal";
+import { Pencil, Mail, Wallet, Zap, Clock } from 'lucide-react';
 
 
 interface UserProfile {
